38073eb37f4f1257e3784b188819b47156140b53
Matheus Martins <116220500+MatheusDev7@users.noreply.github.com>
2023-04-24 20:15:26 -0300

    Fazendo mais alguns exercícios com JavaScript.

--- aula13/ex014/script.js
+++ aula13/ex014/script.js
@@ -5,10 +5,13 @@ function carregar(){
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12){
-        img.src = 'imagens/dia.png'
-    }else if (hora >= 12 && hora < 18){
+        img.src = 'imagens/manha.png'
+        window.document.body.style.background = '#b197e3'
+    }else if (hora >= 12 && hora <= 18){
         img.src = 'imagens/tarde.png'
+        window.document.body.style.background = '#966065'
     }else{
         img.src = 'imagens/noite.png'
+        window.document.body.style.background = '#19193d'
     }
 }
