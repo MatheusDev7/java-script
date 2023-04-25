f66dc302a755304b551033614eee8c631ef8ad77
Matheus Martins <116220500+MatheusDev7@users.noreply.github.com>
2023-04-25 00:59:23 -0300

    .

--- aula13/ex014/script.js
+++ aula13/ex014/script.js
@@ -3,15 +3,20 @@ function carregar(){
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
+
     if (hora >= 0 && hora < 12){
         img.src = 'imagens/manha.png'
         window.document.body.style.background = '#b197e3'
+        msg.innerHTML = `Agora são ${hora} horas da manhã.`
+
     }else if (hora >= 12 && hora <= 18){
         img.src = 'imagens/tarde.png'
         window.document.body.style.background = '#966065'
+        msg.innerHTML = `Agora são ${hora} horas da tarde.`
+        
     }else{
         img.src = 'imagens/noite.png'
         window.document.body.style.background = '#19193d'
+        msg.innerHTML = `Agora são ${hora} horas da noite.`
     }
 }
--- aula13/ex015/script.js
+++ aula13/ex015/script.js
@@ -13,29 +13,30 @@ function verificar(){
         img.setAttribute('id', 'foto')
         if (fsex[0].checked){
             genero = 'Homem'
-            res.innerHTML = `Detectamos um ${genero} com ${idade} anos.`
+            res.innerHTML = `Detectamos um ${genero} que teria ${idade} anos.`
             if (idade >= 0 && idade < 10){
-                
+                img.setAttribute('src', 'imagens/foto-bebe-m.png')
             }else if (idade < 21){
-
+                img.setAttribute('src', 'imagens/foto-jovem-m.png')
             }else if (idade < 50){
-
+                img.setAttribute('src', 'imagens/foto-adulto-m.png')
             }else{
-
+                img.setAttribute('src', 'imagens/foto-idoso-m.png')
             }
         }else if (fsex[1].checked){
             genero = 'Mulher'
-            res.innerHTML = `Detectamos uma ${genero} com ${idade} anos.`
+            res.innerHTML = `Detectamos uma ${genero} que teria ${idade} anos.`
             if (idade >= 0 && idade < 10){
-                
+                img.setAttribute('src', 'imagens/foto-bebe-f.png')
             }else if (idade < 21){
-
+                img.setAttribute('src', 'imagens/foto-jovem-f.png')
             }else if (idade < 50){
-
+                img.setAttribute('src', 'imagens/foto-adulto-f.png')
             }else{
-                
+                img.setAttribute('src', 'imagens/foto-idoso-f.png')
             }
         }
         res.style.textAlign = 'center'
+        res.appendChild(img)
     }
 }
